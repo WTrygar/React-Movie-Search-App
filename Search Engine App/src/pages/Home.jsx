35b87aa6046ea0d85644c9b0a3180385b3f3190b
WTrygar <chandelier.wt@gmail.com>
2025-03-12 21:56:08 +0100

    working searchbar

--- Search Engine App/src/pages/Home.jsx
+++ Search Engine App/src/pages/Home.jsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import MovieCard from "../components/MovieCard";
 
 function Home() {
+  const [searchQuery, setSearchQuery] = useState("");
+
   const movies = [
     { id: 1, title: "John Wick", release_date: 2020 },
     { id: 2, title: "John Wick 2", release_date: 2021 },
@@ -8,7 +11,11 @@ function Home() {
     { id: 4, title: "John Wick 4", release_date: 2023 },
   ];
 
-  const handleSearch = () => {};
+  const handleSearch = (e) => {
+    e.preventDefault();
+    alert(searchQuery);
+    setSearchQuery("");
+  };
 
   return (
     <div className="home">
@@ -17,15 +24,20 @@ function Home() {
           type="text"
           placeholder="Search for movies..."
           className="search-input"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
         />
         <button type="submit" className="search-button">
           Search
         </button>
       </form>
       <div className="movies-grid">
-        {movies.map((movie) => (
-          <MovieCard movie={movie} key={movie.id} />
-        ))}
+        {movies.map(
+          (movie) =>
+            movie.title.toLowerCase().startsWith(searchQuery) && (
+              <MovieCard movie={movie} key={movie.id} />
+            )
+        )}
       </div>
     </div>
   );
