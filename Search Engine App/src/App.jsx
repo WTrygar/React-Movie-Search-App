77650eb65d6e30c8e05d22dd272d5b115ac0efbf
WTrygar <chandelier.wt@gmail.com>
2025-03-13 15:41:29 +0100

    Routes

--- Search Engine App/src/App.jsx
+++ Search Engine App/src/App.jsx
@@ -1,13 +1,16 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Favourites from "./pages/Favourites";
 import Home from "./pages/Home";
 
 function App() {
-  const movieNumber = 1;
-
   return (
-    <>
-      <Home />
-    </>
+    <main className="main-content">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/favourites" element={<Favourites />} />
+      </Routes>
+    </main>
   );
 }
 
