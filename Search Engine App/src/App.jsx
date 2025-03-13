61b62544e387b0e8fbf5451c6ecdeeabe52321b4
WTrygar <chandelier.wt@gmail.com>
2025-03-13 16:05:47 +0100

    Navbar

--- Search Engine App/src/App.jsx
+++ Search Engine App/src/App.jsx
@@ -1,16 +1,20 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import NavBar from "./components/NavBar";
 import Favourites from "./pages/Favourites";
 import Home from "./pages/Home";
 
 function App() {
   return (
-    <main className="main-content">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/favourites" element={<Favourites />} />
-      </Routes>
-    </main>
+    <div>
+      <NavBar />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favourites" element={<Favourites />} />
+        </Routes>
+      </main>
+    </div>
   );
 }
 
